347fa2f5c88bb71c2a70736e973f13028fa61509
Kokil Singh <kokildeepti@gmail.com>
2023-10-26 23:41:23 +0530

    First Part Procedure and Manifest File Added

---
+++ index.py
@@ -0,0 +1,15 @@
+#Import libraries
+
+##### Make upload button for Methylation data File********************
+
+##### Make Template for Average Beta Values
+# Make Download Button for Avg Beta Values Template ********************
+# Make Upload Button For Avg Values that follow template *****************
+
+
+##### Make sure 450K array Manifest file is included in folder
+# Done
+
+##### Submit button *******************
+
+#/******************* First Part Done ***********************/
